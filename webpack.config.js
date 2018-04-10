0e66dd15ef06e190962728f01b1b25da892891b9
alexterua <alexter.od.ua@gmail.com>
2018-04-10 22:01:08 +0300

    dev and prod env

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     'index': PATHS.source + '/pages/index/index.js',
     'blog': PATHS.source + '/pages/blog/blog.js'
@@ -39,8 +39,25 @@ module.exports = {
         }
       }
     ]
-  },
+  }
+};
+
+const developmentConfig = {
   devServer: {
-    stats: 'errors-only'
+    stats: 'errors-only',
+    port: 9000
+  }
+}
+
+module.exports = function(env) {
+  if(env === 'production') {
+    return common;
+  }
+  if(env === 'development') {
+    return Object.assign(
+      {},
+      common,
+      developmentConfig
+    )
   }
 };
