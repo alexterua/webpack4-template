dee5229762f2d480c0a5c399ce5ddfa09e02da62
alexterua <alexter.od.ua@gmail.com>
2018-04-11 18:48:37 +0300

    splitChunksPlugin don't compile css in common

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,19 @@ const common = merge([
     entry: {
       'index': PATHS.source + '/pages/index/index.js',
       'blog': PATHS.source + '/pages/blog/blog.js'
-    },    
+    },
+    // splitChunksPlugin вместо CommonsChunkPlugin
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            name: "common",
+            chunks: "initial"
+          }
+        }
+      }
+    },
+    
     output: {
       path: PATHS.build,
       filename: 'js/[name].js'
@@ -36,10 +48,9 @@ const common = merge([
       }),
       /*new webpack.optimize.CommonsChunkPlugin({
         name: 'common'
-      })*/
-      new webpack.optimize.splitChunks({
-        name: 'common'
-      })
+      })*/    
+        
+      
     ]
   },
   pug()  
